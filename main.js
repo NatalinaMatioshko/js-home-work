954142596753be157db3bab053f50f6461988128
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-10-13 18:20:58 +0300

    add initial files, create variables

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+alert(`Hello user!`);
+
+let userFromUkraine = confirm("Are you form Ukraine?");
+console.log(userFromUkraine);
+
+let userFirstName = prompt("Enter your name ?");
+alert(`Hello my friend ${userFirstName}`);
+console.log(userName);
