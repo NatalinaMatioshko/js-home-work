78f45e4a16010b781bcd89d2b7dece39ebedddba
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-10-14 20:23:37 +0300

    add circle area, distance, currency converter

--- main.js
+++ main.js
@@ -14,10 +14,13 @@
 // camelCase, snake_case, PascalCase, kebab-case
 // 4username
 
-/*
-  User`s name, age, perimeter of square
-    */
+/*  User`s name, age, perimeter of square    */
+
 let userFirstName = prompt("What is your name ?");
+
+// if (!userFirstName) {
+//   alert("You didn’t enter your name!");
+// }
 
 alert(`Hello my friend ${userFirstName}`);
 
@@ -25,6 +28,9 @@ let userYearOfBirth = prompt("What is your year of birth?");
 const currentYear = new Date().getFullYear();
 let userAge = currentYear - Number(userYearOfBirth);
 
+// if (isNaN(userYearOfBirth) || userYearOfBirth > currentYear) {
+//   alert("Please enter a valid year!");
+// }
 alert(`You are ${userAge} years old`);
 
 let squareSide = prompt("Enter length of the side of the square?");
@@ -39,10 +45,45 @@ if (isNaN(numberSide)) {
   alert(`Perimeter of a square with side ${squareSide} is ${squarePerimeter}!`);
 }
 
-console.log(userFirstName);
-console.log(userAge);
-console.log(currentYear);
-console.log(numberSide);
-/*
-  User`s s
-    */
+// console.log(userFirstName);
+// console.log(userAge);
+// console.log(currentYear);
+// console.log(numberSide);
+
+/*  Area of a circle    */
+
+let radiusInput = prompt("Enter the radius of the circle");
+let circleRadius = Number(radiusInput);
+
+const PI = Math.PI;
+let area = PI * circleRadius ** 2;
+// let area = PI * Math.pow(circleRadius, 2);
+let roundedArea = area.toFixed(2);
+alert(`The area of a circle with radius ${circleRadius} is ${roundedArea}`);
+
+/*  Speed of travel between cities   */
+
+let distanceInput = prompt("Enter distance to the city (in kilometers)");
+let timeInput = prompt(
+  "Enter the time required to cover this distance (in hours)"
+);
+let distance = Number(distanceInput);
+let time = Number(timeInput);
+
+let speed = distance / time;
+let roundedSpeed = speed.toFixed(2);
+
+alert(
+  `To arrive on time, you need to travel at a speed of ${roundedSpeed} km/h`
+);
+
+/*  currency converter   */
+
+let dollarsInput = prompt("Enter the amount in dollars");
+let dollars = Number(dollarsInput);
+const USD_TO_EUR = 0.93;
+let euros = dollars * USD_TO_EUR;
+let roundedEuros = euros.toFixed(2);
+alert(
+  `${dollars} dollars equals ${roundedEuros} euros at an exchange rate of ${USD_TO_EUR}`
+);
