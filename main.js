36f5fe90eeb2499302e56942fb19ff14c2fe54db
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-10-14 19:22:39 +0300

    add task morma - users name, age, square perimeter

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
 "use strict";
 
-alert(`Hello user!`);
+// alert(`Hello user!`);
 
-let userFromUkraine = confirm("Are you form Ukraine?");
-console.log(userFromUkraine);
+// let userFromUkraine = confirm("Are you form Ukraine?");
+// console.log(userFromUkraine);
 
-let userFirstName = prompt("Enter your name ?");
-alert(`Hello my friend ${userFirstName}`);
-console.log(userFirstName);
+// let userAge = 18;
 
-let userAge = 18;
+// userName;
+// _userName;
+// $userName;
 
-userName;
-_userName;
-$userName;
-
+// camelCase, snake_case, PascalCase, kebab-case
 // 4username
+
+/*
+  User`s name, age, perimeter of square
+    */
+let userFirstName = prompt("What is your name ?");
+
+alert(`Hello my friend ${userFirstName}`);
+
+let userYearOfBirth = prompt("What is your year of birth?");
+const currentYear = new Date().getFullYear();
+let userAge = currentYear - Number(userYearOfBirth);
+
+alert(`You are ${userAge} years old`);
+
+let squareSide = prompt("Enter length of the side of the square?");
+let numberSide = parseFloat(squareSide);
+
+if (isNaN(numberSide)) {
+  alert("Please enter a number!");
+} else if (numberSide <= 0) {
+  alert("The side of the square must be greater than zero!");
+} else {
+  let squarePerimeter = 4 * numberSide;
+  alert(`Perimeter of a square with side ${squareSide} is ${squarePerimeter}!`);
+}
+
+console.log(userFirstName);
+console.log(userAge);
+console.log(currentYear);
+console.log(numberSide);
+/*
+  User`s s
+    */
--- readme.md
+++ readme.md
@@ -1,15 +1,45 @@
-## #1 завдання
+## Minimun
 
-підключено script вбудованим способом, в head та пере закриваючим тегом body
+## #1 task
 
-## #2 завдання
+script connected in a built-in way, in the head and closing body tag
 
-створено 4 змінні
+## #2 task
 
-## #3 завдання
+4 variables created
 
-застосовано 2 способи коментування
+## #3 task
 
-## #3 завдання
+2 commenting methods applied
 
-стилі написання імен
+## #4 task
+
+naming styles
+
+## Norma
+
+## #1 task
+
+Ask for the user's name and display “Hello, _name_” in response.
+
+## #2 task
+
+Ask for the user's year of birth, calculate his/her age, and display the result. Specify the current year in the code as a constant.
+
+## #3 task
+
+Ask the user for the length of the side of the square and display the perimeter of this square.
+
+## Maximum
+
+## #1 task
+
+Ask the user for the radius of the circle and calculate the area of that circle.
+
+## #2 task
+
+Ask the user for the distance in kilometers between two cities and how many hours they want to get there. Calculate the speed at which they need to travel to arrive on time.
+
+## #3 task
+
+Implement a currency converter. The user enters dollars, and the program converts them to euros. The exchange rate is stored in a constant.
