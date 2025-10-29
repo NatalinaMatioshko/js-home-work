d65c9b2f7705309ee2af2c082c5da72bc70196d5
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-10-29 18:49:01 +0200

    add three more elements

--- main.js
+++ main.js
@@ -23,3 +23,21 @@ main.appendChild(newSection);
 const newTitle = document.createElement("h2");
 newTitle.textContent = "It`s a new title in a new section";
 newSection.appendChild(newTitle);
+
+// ===============================================================
+const paragraph = document.createElement("p");
+paragraph.textContent = "It is the text.";
+paragraph.classList.add("text-block");
+
+const button = document.createElement("button");
+button.textContent = "Click me";
+button.classList.add("btn");
+
+const image = document.createElement("img");
+image.src = "./img/favicon.svg";
+image.alt = "JS";
+image.classList.add("photo");
+
+newSection.appendChild(paragraph);
+newSection.appendChild(button);
+newSection.appendChild(image);
